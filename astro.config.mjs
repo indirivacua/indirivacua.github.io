aefbb1a0f8e06c7042bfdc3bc413ee18aac547c4
Oscar Stanchi <indirivacua@hotmail.com>
2023-10-27 14:16:54 -0300

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'https://indirivacua.github.io',
+  base: 'indirivacua.github.io',
   integrations: [mdx(), sitemap(), tailwind()]
 });
